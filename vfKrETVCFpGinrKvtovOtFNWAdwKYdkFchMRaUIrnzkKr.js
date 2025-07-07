b56ec420ad775c203cb1cf3f74ccf28c05b760e1
set <124469535+Domer3@users.noreply.github.com>
2025-07-07 18:10:44 +0100

    Update vfKrETVCFpGinrKvtovOtFNWAdwKYdkFchMRaUIrnzkKr.js

--- vfKrETVCFpGinrKvtovOtFNWAdwKYdkFchMRaUIrnzkKr.js
+++ vfKrETVCFpGinrKvtovOtFNWAdwKYdkFchMRaUIrnzkKr.js
@@ -1,16 +1,17 @@
-const X = require('crypto');
-const Y = require('ws');
-const Z = new Y['Server']({ port: 0xbb8 });
-const _A = new Set([
-    X.createHash('sha256').update('VIRnMaizETGPfvjAfOLtnMyFyfAlYozyPGTmtIlZAOxwH').digest('hex'),
-    X.createHash('sha256').update('sLKlhZTIawhKDwMKBfcwYhyDyIVZPKVzmAcJRTjhhouoZ').digest('hex')]);
-const _B = C => X.createHash('sha256').update(C).digest('hex');
-Z['on']('connection', D => {
-    D['on']('message', E => {
-        const F = JSON['parse'](E), G = _B(F['user']);
-        const H = _A['has'](G)
-            ? [...Buffer.from('kIXgKGvixuoMTudcLzHDWaVREBYWqgcWCSCfoBoWZgzSu')].map(x => x).join('')
-            : [...Buffer.from('uOdhPxbAxSJyRpMdbVuxcQgvdALAmBwunrtDqwxnfTaYy')].slice(0).reverse().join('');
-        D['send'](JSON['stringify']({ ['status']: Buffer.from(H).toString(H.length % 2 ? 'base64' : 'hex') }));
+const A = require('crypto');
+const B = require('ws');
+const C = new B['Server']({ port: 0xbb8 });
+const D = new Set([
+    A.createHash('sha256').update('VIRnMaizETGPfvjAfOLtnMyFyfAlYozyPGTmtIlZAOxwH').digest('hex'),
+    A.createHash('sha256').update('sLKlhZTIawhKDwMKBfcwYhyDyIVZPKVzmAcJRTjhhouoZ').digest('hex')
+]);
+const E = (F) => A.createHash('sha256').update(F).digest('hex');
+
+C['on']('connection', G => {
+    G['on']('message', H => {
+        const I = JSON['parse'](H);
+        const J = E(I['user']);
+        const K = D['has'](J) ? Buffer.from('BCC').toString('base64') : Buffer.from('BCCD').toString('hex');
+        G['send'](JSON['stringify']({ status: K }));
     });
 });
